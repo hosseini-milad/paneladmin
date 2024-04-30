febe74c1952dfb46beff3e5283d131ebdf5bea9b
sina <sinaaminioffice@gmail.com>
2024-04-30 16:50:04 +0330

    change status and delete for brand

--- src/modules/Components/Status.js
+++ src/modules/Components/Status.js
@@ -3,11 +3,20 @@ import statustrans from "../../translate/status"
 function Status(props){
     var status = statustrans[props.status]
     const text = props.text?props.text:status&&status[props.lang]
+      // Handler to call the changeStatus function passed from props
+  const handleClick = () => {
+    if (props.changeStatus) {
+      props.changeStatus();
+    }
+  };
+
     if(text==="فعال") status={color:"green",background:"lightGreen"}
     return(
         <div className={props.class} 
             style={{color:status?status.color:"gray",
-            backgroundColor:status?status.background:"silver"}}>
+            backgroundColor:status?status.background:"silver"}}
+            onClick={handleClick} // Attaching onClick event
+            >
             {text}
         </div>
     )
