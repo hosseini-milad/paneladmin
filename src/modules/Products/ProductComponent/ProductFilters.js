a6694ab51abfdd5258eff5a344972e0c27b6f36e
milad <milad.cia@gmail.com>
2024-06-23 18:51:28 +0330

    filters

--- src/modules/Products/ProductComponent/ProductFilters.js
+++ src/modules/Products/ProductComponent/ProductFilters.js
@@ -29,11 +29,6 @@ function ProductFilters(props) {
           direction={props.lang.dir}
           action={(e) => handleFilterChange("title", e)}
         />
-        <StyleInput
-          title="SKU"
-          direction={props.lang.dir}
-          action={(e) => handleFilterChange("sku", e)}
-        />
         <StyleSelect
           title={tabletrans.productCategory[lang.lang]}
           direction={props.lang.dir}
