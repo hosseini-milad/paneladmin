c51a538d9307f2d9602f229abcd93e53ec8193f7
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2024-07-22 12:43:00 +0330

    print garantee fixed

--- src/index.js
+++ src/index.js
@@ -60,6 +60,9 @@ import Discount from './pages/Discount';
 import AccessUser from './pages/access-user';
 import Garantee from './pages/Garantee';
 import StockHolder from './modules/Orders/StockOrder/StockHolder';
+import PrintGuranteeStock from "./modules/Orders/printGuranteeStock";
+import PrintGuranteeRx from "./modules/Orders/printGuranteeRx";
+
 
 const cookies = new Cookies();
 const style = document.getElementById('style-direction');
@@ -103,6 +106,8 @@ root.render(
         <Route path="/access/detail/:profileId" element={<Layout><ProfileAdd lang={lang}/></Layout>}/>
         <Route path="/filter" element={<Layout><FilterHolder lang={lang}/></Layout>}/>
         <Route path="/filter/detail/:filtereId" element={<Layout><FilterAdd lang={lang}/></Layout>}/>
+        <Route path="/print-guaranteeStock/:orderId" element={<PrintGuranteeStock />} />
+        <Route path="/print-guaranteeRx/:orderId" element={<PrintGuranteeRx />} />
 
         <Route path="/orders" element={<Layout><Orders lang={lang}/></Layout>}/>
         <Route path="/cancelorders" element={<Layout><CanOrders lang={lang}/></Layout>}/>
--- src/pages/garantee.js
+++ src/pages/garantee.js
@@ -1,6 +1,7 @@
 import React ,{ useState }from 'react'
 import { useEffect } from "react";
 import Cookies from "universal-cookie";
+import { TextField } from "@material-ui/core"
 
 import errortrans from "../translate/error";
 import tabletrans from "../translate/tables";
@@ -11,6 +12,8 @@ const cookies = new Cookies();
 
 
 const Garantee = (props) => {
+  var regex = new RegExp("^[a-zA-Z0-9,._ ]+$");
+
   const direction = props.lang?props.lang.dir:errortrans.defaultDir;
   const lang = props.lang?props.lang.lang:errortrans.defaultLang;
   const [RxStock,setRxStock] = useState("")
@@ -18,10 +21,11 @@ const Garantee = (props) => {
   const [loading,setLoading] = useState(0)
   const [search,setSearch] = useState('')
   const [content,setContent] = useState("")
+  const [Customer,setCustomer] = useState("")
 
   const token=cookies.get(env.cookieName)
-
-
+  
+  
   useEffect(() => {
     setLoading(1)
     const body={
@@ -53,9 +57,8 @@ const Garantee = (props) => {
   const sendGarantee =()=>{
   setLoading(1)
     const body={
-      orderNo:OrderID,
-      guranteeName:content.userData.cName,
-      guranteePrice:content.price,
+      orderNo:content.orderData.rxOrderNo,
+      guranteeName:Customer,
     }
     const postOptions={
         method:'post',
@@ -63,12 +66,14 @@ const Garantee = (props) => {
         "x-access-token":token&&token.token,"userId":token&&token.userId},
         body:JSON.stringify(body)
       }
-
       fetch(env.siteApi + "/panel/order/setGuranteeOrder",postOptions)
       .then(res => res.json())
       .then(
         (result) => {
+        const OrderNum =content.orderData.rxOrderNo
+        const OrderType = (OrderNum.includes("S"))?"stock":"rx"
         setLoading(0)
+        {RxStock=="stock"?window.open("/print-guaranteeStock/"+OrderNum,'_blank'):window.open("/print-guaranteeRx/"+OrderNum,'_blank')}
       },
         (error) => {
         setLoading(0);
@@ -76,7 +81,6 @@ const Garantee = (props) => {
       }
       );
   }
-
 
 if(!content)
   return(
@@ -106,17 +110,15 @@ if(!content)
               <div className={`tab-btn ${RxStock=="rx"?"active-tab":""} `} >Rx</div>
             </div>
             <div className="brand-container">
-              <StyleInput
-              title={tabletrans.brand[lang]}
-              direction={lang.dir}
-              defaultValue={content.lData&&content.lData.brandName}
-              />
+              <div className="fake-input">
+                <p>{content.lData&&content.lData.brandName}</p>
+                <span>{tabletrans.brand[lang]}</span>
+              </div>
+              <div className="fake-input">
+                <p>{content.lData&&content.lData.material}</p>
+                <span>{tabletrans.material[lang]}</span>
+              </div>
               
-              <StyleInput
-                title={tabletrans.material[lang]}
-                direction={lang.dir}
-                defaultValue={content.lData&&content.lData.material}
-              />
               
             </div>
           </div>
@@ -127,83 +129,83 @@ if(!content)
         <div className="lathe-container">
           <div className="input-index-wrapper">
             <p className="title">OD</p>
-            <StyleInput
-              title="Sphere"
-              direction={lang.dir}
-              defaultValue={content.lData&&content.lData.sph}
-            />
-            <StyleInput
-              title="Cylinder"
-              direction={lang.dir}
-              defaultValue={content.lData&&content.lData.cyl}
-            />
-            <StyleInput
-              title="Axis"
-              direction={lang.dir}
-            />
-            <StyleInput
-              title="PD"
-              direction={lang.dir}
-            />
-            {RxStock=="rx"?<StyleInput
-              title="Addition"
-              direction={lang.dir}
-            />:<></>}
+            
+            <div className="fake-input">
+                <p>{content.lData&&content.lData.sph}</p>
+                <span>Sphere</span>
+            </div>
+            <div className="fake-input">
+                <p>{content.lData&&content.lData.cyl}</p>
+                <span>Cylinder</span>
+            </div>
+            <div className="fake-input">
+                <p>{content.lData&&content.lData.axis}</p>
+                <span>Axis</span>
+            </div>
+            <div className="fake-input">
+                <p>{content.lData&&content.lData.pd}</p>
+                <span>PD</span>
+            </div>
+            
+            {RxStock=="rx"?
+            <div className="fake-input">
+              <p>{content.lData&&content.lData.add}</p>
+              <span>Add</span>
+            </div>:<></>}
           </div>
           <div className="input-index-wrapper">
-          <p className="title">OS</p>
-            <StyleInput
-              title="Sphere"
-              direction={lang.dir}
-              defaultValue={content.rData&&content.rData.sph}
-            />
-            <StyleInput
-              title="Cylinder"
-              direction={lang.dir}
-              defaultValue={content.rData&&content.rData.cyl}
-            />
-            <StyleInput
-              title="Axis"
-              direction={lang.dir}
-            />
-            <StyleInput
-              title="PD"
-              direction={lang.dir}
-            />
-            {RxStock=="rx"?<StyleInput
-              title="Addition"
-              direction={lang.dir}
-            />:<></>}
+            <p className="title">OS</p>
+            
+            <div className="fake-input">
+                <p>{content.rData&&content.rData.sph}</p>
+                <span>Sphere</span>
+            </div>
+            <div className="fake-input">
+                <p>{content.rData&&content.rData.cyl}</p>
+                <span>Cylinder</span>
+            </div>
+            <div className="fake-input">
+                <p>{content.rData&&content.rData.axis}</p>
+                <span>Axis</span>
+            </div>
+            <div className="fake-input">
+                <p>{content.rData&&content.rData.pd}</p>
+                <span>PD</span>
+            </div>
+            
+            {RxStock=="rx"?
+            <div className="fake-input">
+              <p>{content.rData&&content.rData.add}</p>
+              <span>Add</span>
+            </div>:<></>}
           </div>
         </div>
         <div className="info-container">
           <div className="container">
-            <StyleInput
-                title="نام بیمار"
-                direction={lang.dir}
-                defaultValue={content.userData&&content.userData.cName}
+            
+            <TextField label="(به انگلیسی)نام مشتری" id="Customer"
+                  value = {Customer?Customer:""}
+                  onChange={(e)=>(regex.test(e.target.value)&&
+                    e.target.value.length<19||e.target.value==='')&&
+                    (setCustomer(e.target.value),console.log(e.target.value))}
+                      variant="outlined"
             />
-            <StyleInput
-                title="شماره قبض مشتری"
-                direction={lang.dir}
-            />
-            <StyleInput
-                title="هزینه تراش به تومان"
-                direction={lang.dir}
-                defaultValue={content.price&&content.price}
-            />
+            
+            <div className="fake-input">
+                <p>{content.price&&content.price}</p>
+                <span>هزینه تراش به تومان</span>
+            </div>
           </div>
-          <StyleInput
+          {/* <StyleInput
               title="توضیحات"
               direction={lang.dir}
-          />
-          <div className="dense-btn">
+          /> */}
+          {/* <div className="dense-btn">
             <input className="switch-input" type="checkbox" id="switch" />
             <label className="switch-label" htmlFor="switch"></label>
             <p>فوری</p>
-          </div>
-          <button className="submit">ذخیره</button>
-          <button className="submit">ذخیره و چاپ</button>
+          </div> */}
+          <button onClick={()=>sendGarantee()} className="submit">ذخیره و چاپ</button>
         </div>
       </div>
     </div>
