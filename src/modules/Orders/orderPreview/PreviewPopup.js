2a53f10b0084b6e1e8e3175eea2a66aa06a5af1c
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2024-08-01 18:14:35 +0330

    preview Rx

---
+++ src/modules/Orders/orderPreview/PreviewPopup.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+import env from "../../../env"
+import PreViewRX from "./PreViewRX"
+function PreviewPopup(props){
+    //console.log(data)
+    return(
+    <section className="delete-modal" style={{direction:"rtl"}}>
+        <div className="modal-backdrop show-modal">
+            <div className="task-popup fullPopUp">
+                <i className="fa fa-remove closeModal" 
+                    onClick={()=>props.close(0)}></i>
+                <PreViewRX Rxnum={props.Rxnum}/>
+            </div>
+            
+        </div>
+    </section>
+    )
+}
+export default PreviewPopup
