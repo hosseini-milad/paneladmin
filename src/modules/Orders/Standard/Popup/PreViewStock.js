f54d045a303b648696a808c36af290bbd1e588b4
Amir Hossein Salimi <ah.salimi1431@gmail.com>
2024-08-28 17:21:30 +0330

    accessories table

--- src/modules/Orders/Standard/Popup/PreViewStock.js
+++ src/modules/Orders/Standard/Popup/PreViewStock.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 // import ButtonLoader from "../../Components/BtnLoader";
-import env, { normalPrice, normalPriceCount, purePrice, sumPrice, sumPriceNew , rxFindCount} from "../../../env";
+import env, { normalPrice, normalPriceCount, purePrice, sumPrice, sumPriceNew , rxFindCount} from "../../../../env";
 
 
 // import Autocomplete from '@material-ui/lab/Autocomplete';
 // import TextField from '@material-ui/core/TextField';
-import PreviewStockTable from "./PreviewStockTable";
-import PreviewStTable from "../Standard/Popup/PreviewStTable";
+import PreviewStockTable from "./PreviewStTable";
+import PreviewStTable from "./PreviewStTable";
 import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
@@ -16,36 +16,25 @@ const token=cookies.get(env.cookieName)
 function PreViewStock(props){
     const url = props.Stocknum
     const [Content,setContent] = useState('')
-    const [AcContent,setAcContent] = useState('')
     const [actions,setActions] = useState('')
     const [orderNo,setOrderNo] = useState('')
     
 
     useEffect(() => {
-      setContent("")
-      setActions("")
-      setOrderNo("")
-      setAcContent("")
       var postOptions={
           method:'post',
           headers: {'Content-Type': 'application/json',
               "x-access-token":token&&token.token,"userid":token&&token.userId},
-          body:JSON.stringify(url.includes("S")?{stockOrderNo:url}:{orderNo:url})
+          body:JSON.stringify({stockOrderNo:url})
         }
-    fetch(env.siteApi +(url.includes("S")? "/order/fetch-stock":"/xtra/fetch-order"),postOptions)
+    fetch(env.siteApi + "/order/fetch-stock",postOptions)
     .then(res => res.json())
     .then(
       (result) => {
-        if(url.includes("S")){
         setContent(result.classResult)
         setActions(result.statusActions)
         setOrderNo(result.stockOrderNo)
-        }else{
-          setAcContent(result)
         }
-      
-      
-      }
         )
         
       },
@@ -56,10 +45,8 @@ function PreViewStock(props){
   
     return(
         <div class="preview-rx">
-          {url.includes("S")?<PreviewStockTable Stocknum={url} content={Content} orderNo={orderNo}
-            access={props.access} actions={actions} token={token}/>:
-          <PreviewStTable AccNum={url} content={AcContent} orderNo={orderNo}
-            access={props.access} actions={actions} token={token}/>}
+          <PreviewStockTable Stocknum={url} content={Content} orderNo={orderNo}
+            access={props.access} actions={actions} token={token}/>
         </div>
         
     )
